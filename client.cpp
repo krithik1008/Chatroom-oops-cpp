d454c7cb0b6d8143786e7e420c52a0fbf8d3b8ed
krithik <krithik1008@gmail.com>
2021-10-23 01:40:21 +0530

    added comments related to socket programming and oops

--- client.cpp
+++ client.cpp
@@ -9,7 +9,7 @@
 #include <thread>
 #include <signal.h>
 #include <mutex>
-#define MAX_LEN 200
+#define MAX_LEN 200					//max length of message
 #define NUM_COLORS 6
 using namespace std;
 
@@ -17,47 +17,37 @@ thread t_send, t_recv;
 string def_col="\033[0m";
 string colors[]={"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
 
-class client
+class client							//class to hold client data members and functions
 {
 	private:
-		struct sockaddr_in client_add;
-		char name[MAX_LEN];
-		static int client_socket;
-		static bool exit_flag;
+		struct sockaddr_in client_add;	//predefined struct containing address information of sockets
+		char name[MAX_LEN];				//holds name of the client
+		static int client_socket;		//holds socket descriptor of the client 
+		static bool exit_flag;			//used to end client socket connection
 	public:
-		client()
+		client()						//constructor to initialize required parameters of the client address
 		{
-			client_add.sin_family=AF_INET;
-			client_add.sin_port=htons(10000); // Port no. of server
-			client_add.sin_addr.s_addr=INADDR_ANY;
-			//client.sin_addr.s_addr=inet_addr("127.0.0.1"); // Provide IP address of server
-			bzero(&client_add.sin_zero,0);
+			client_add.sin_family=AF_INET;		//specifying IP address family (IPv4)
+			client_add.sin_port=htons(10000); 	// Port no. of server
+			client_add.sin_addr.s_addr=INADDR_ANY;	//allows connecting to any available ip address
+			bzero(&client_add.sin_zero,0);		//array buffer, usually set to 0
 		}
-		static int modify_client_socket(){
+		static int modify_client_socket(){		//overloaded static function to update client_socket
 			return client_socket;
 		}
-		static void modify_client_socket(int val){
-			client_socket=val;
+		static void modify_client_socket(int v){//overloaded static function to return client_socket value
+			client_socket=v;
 		}
-		static bool modify_exit_flag(){
+		static bool modify_exit_flag(){			//overloaded static function to update exit_flag
 			return exit_flag;
 		}
-		static void modify_exit_flag(bool val){
-			exit_flag=val;
+		static void modify_exit_flag(bool v){ 	//overloaded static function to return exit_flag value
+			exit_flag=v;
 		}
-		// Handler for "Ctrl + C"
-		static void catch_ctrl_c(int signal) 
-		{
-			char str[MAX_LEN]="#exit";
-			send(client::modify_client_socket(),str,sizeof(str),0);
-			//exit_flag=true;
-			t_send.detach();
-			t_recv.detach();
-			close(client::modify_client_socket());
-			exit(signal);
-		}
+		//creates an IPv4 (AF_INET) TCP socket endpoint for the client (SOCK_STREAM => TCP)
 		void connect_socket()
 		{
+			//returns socket descriptor if connection successful, else returns -1
 			client::modify_client_socket(socket(AF_INET,SOCK_STREAM,0));
 			if(client::modify_client_socket()==-1)
 			{
@@ -65,6 +55,7 @@ class client
 				exit(-1);
 			}
 		}
+		//connect is called by client to connect to the server at the specified port 
 		void establish_connection()
 		{
 			if((connect(client::modify_client_socket(),(struct sockaddr *)&client_add,sizeof(struct sockaddr_in)))==-1)
@@ -76,48 +67,50 @@ class client
 		void new_user()
 		{
 			cout<<"Enter your name : ";
-			cin.getline(name,MAX_LEN);
+			cin.getline(name,MAX_LEN);			//input the name of the client (user)
+			//send client_socket and name of the client to server
 			send(client::modify_client_socket(),name,sizeof(name),0);
-
 			cout<<colors[NUM_COLORS-1]<<"\n\t  ====== Welcome to the chat-room ======   "<<endl<<def_col;
 		}
-		// Send message to everyone
+		// Send message to all clients via the server
 		static void send_message(int client_socket)
 		{
 			while(1)
 			{
 				cout<<colors[1]<<"You : "<<def_col;
 				char str[MAX_LEN];
-				cin.getline(str,MAX_LEN);
-				send(client_socket,str,sizeof(str),0);
+				cin.getline(str,MAX_LEN);				//getting message to be transmitted 
+				send(client_socket,str,sizeof(str),0);	//send message to the server
+				//To diconnect the client and exit chatroom
 				if(strcmp(str,"#exit")==0)
 				{
-					client::modify_exit_flag(true);
+					client::modify_exit_flag(true);		//set exit_flag to true
 					t_recv.detach();	
-					close(client_socket);
+					close(client_socket);				//close the socket connection with server
 					return;
 				}	
 			}		
 		}
 
-		// Receive message
+		// Receive message from the server
 		static void recv_message(int client_socket)
 		{
 			char back_space=8;
 			while(1)
 			{
-				if(client::modify_exit_flag())
+				if(client::modify_exit_flag())	//if exit_flag is set to true, then return
 					return;
 				char name[MAX_LEN], str[MAX_LEN];
 				int color_code;
-				int bytes_received=recv(client_socket,name,sizeof(name),0);
-				if(bytes_received<=0)
+				int bytes_received=recv(client_socket,name,sizeof(name),0);	//name of sender received from the server
+				if(bytes_received<=0)			//if no data received then continue 
 					continue;
-				recv(client_socket,&color_code,sizeof(color_code),0);
-				recv(client_socket,str,sizeof(str),0);
+				recv(client_socket,&color_code,sizeof(color_code),0);	//receive color of the sender from server
+				recv(client_socket,str,sizeof(str),0);					//receive message of the sender from the server
 				for(int i=0; i<6; i++)
-					cout<<back_space;
-				if(strcmp(name,"#NULL")!=0)
+					cout<<back_space;		
+				//print the received message along with the name of the sender 				
+				if(strcmp(name,"#NULL")!=0)	
 					cout<<colors[color_code%NUM_COLORS]<<name<<" : "<<def_col<<str<<endl;
 				else
 					cout<<colors[color_code%NUM_COLORS]<<str<<endl;
@@ -129,12 +122,24 @@ class client
 int client::client_socket=0;
 bool client::exit_flag=false;
 
+// Handler for "Ctrl + C" to close the client chat
+void catch_ctrl_c(int signal) 
+{
+	char str[MAX_LEN]="#exit";
+	//transmit exit message if user exits chatroom
+	send(client::modify_client_socket(),str,sizeof(str),0);
+	t_send.detach();
+	t_recv.detach();
+	close(client::modify_client_socket());				//close the socket connection with server
+	exit(signal);
+}
+
 int main()
 {
-	client *user=new client;
+	client *user=new client;			//creating object pointer of class client 
 	user->connect_socket();
 	user->establish_connection();
-	signal(SIGINT, client::catch_ctrl_c);
+	signal(SIGINT, catch_ctrl_c);
 	user->new_user();
 
 	thread t1(client::send_message, client::modify_client_socket());
--- server.cpp
+++ server.cpp
@@ -8,46 +8,48 @@
 #include <unistd.h>
 #include <thread>
 #include <mutex>
-#define MAX_LEN 200
+#define MAX_LEN 200				//max length of message
 #define NUM_COLORS 6
-#define NUM_CLIENTS 5
+#define NUM_CLIENTS 5			//max number of clients on the server
 using namespace std;
 
 string def_col="\033[0m";
 string colors[]={"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m","\033[36m"};
 mutex cout_mtx,clients_mtx;
 
-class server
+class server							//class to hold data members and functions required by the server
 {
 	private:
-	struct sockaddr_in socket_address;
-	static int server_socket;
+	struct sockaddr_in socket_address;	//predefined struct containing address information of sockets
+	static int server_socket;			//holds socket descriptor(int) 		
 	public:
-	server()
+	server()							//constructor to initialize the required server parameters
 	{
-		socket_address.sin_family=AF_INET;
-		socket_address.sin_port=htons(10000);
-		socket_address.sin_addr.s_addr=INADDR_ANY;
-		bzero(&socket_address.sin_zero,0);
+		socket_address.sin_family=AF_INET;			//specifying IP address family (IPv4)
+		socket_address.sin_port=htons(10000);		//assigning port number
+		socket_address.sin_addr.s_addr=INADDR_ANY;	//allows binding to any available ip address 
+		bzero(&socket_address.sin_zero,0);			//array buffer, usually set to 0
 	}
-	static void update_server_socket(int val){
+	static void update_server_socket(int val){	//overloaded static function to update server_socket
 		server_socket=val;
 	}
-	static int update_server_socket(){
+	static int update_server_socket(){			//overloaded static function to return server_socket value
 		return server_socket;
 	}
-	void establish_server()
+	void establish_server()			
 	{
-		server_socket=(socket(AF_INET,SOCK_STREAM,0));
+		server_socket=(socket(AF_INET,SOCK_STREAM,0));	//creates a IPv4 TCP socket endpoint (SOCK_STREAM->TCP)
 		if(server_socket==-1){
 			perror("socket: ");
 			exit(-1);
 		}
+		//bind attaches a port and ip address to an established socket
 		if((bind(server_socket,(struct sockaddr *)&socket_address,sizeof(struct sockaddr_in)))==-1){
 			perror("bind error: ");
 			exit(-1);
 		}
-		if((listen(server_socket,8))==-1){
+		//listen function waits for connection to be established be a client 
+		if((listen(server_socket,8))==-1){	// here 8 clients can be handled in the queue by the server
 			perror("listen error: ");
 			exit(-1);
 		}
@@ -62,20 +64,22 @@ class server
 	}
 };
 int server::server_socket=0;
-int seed=0;
+int seed=0;				//contains the number of clients connected to the server
 
-class terminal
+class terminal			//class to hold details of each client connecting to the server
 {
 	private:
-	int id;
+	int id;				//each client is assigned an id 
 	string name;
-	int socket;
+	int socket;			//holds description of client socket
 	public:
-	thread th;
-	~terminal()
+	thread th;			//each client operates on a seperate thread for simultaneous chatting
+	//destructor to close the client socket connection 
+	~terminal()	
 	{
 		close(socket);
 	}
+	//functions to get and update private data members for terminal class
 	void update_id(int id){
 		this->id=id;
 	}
@@ -96,7 +100,7 @@ class terminal
 	}
 };
 
-terminal* clients[NUM_CLIENTS];
+terminal* clients[NUM_CLIENTS];		//creating an array of terminal object pointers for all new clients
 
 string color(int code);
 void set_name(int id, char name[]);
@@ -110,23 +114,23 @@ int main()
 	for(int i=0;i<NUM_CLIENTS;i++)
 		clients[i]=new terminal;
 
-	server *new_server=new server;
-	new_server->establish_server();	
+	server *new_server=new server;		//creating object opinter of server class
+	new_server->establish_server();		
 
-	struct sockaddr_in client;
-	int client_socket;
+	struct sockaddr_in client;			//temporary sockaddr_in struct to hold addresses of new clients
+	int client_socket;					//hold socket descriptor of new clients 
 	unsigned int len=sizeof(sockaddr_in);
 
 	cout<<colors[NUM_COLORS-1]<<"\n\t  ====== Welcome to the chat-room ======   "<<endl<<def_col;
 
-	while(1)
-	{
+	while(1){
+		//server accepting conncetion request of client using accept(), which returns client socket description
 		if((client_socket=accept(server::update_server_socket(),(struct sockaddr *)&client,&len))==-1)
 		{
 			perror("accept error: ");
 			exit(-1);
 		}
-		seed++;
+		seed++;		//number of clients in server
 		thread t(handle_client,client_socket,seed);
 		lock_guard<mutex> guard(clients_mtx);
 		clients[seed]->update_id(seed);
@@ -141,10 +145,10 @@ int main()
 			clients[i]->th.join();
 	}
 
-	close(server::update_server_socket());
+	close(server::update_server_socket());		//end server socket connection
 	return 0;
 }
-void set_name(int id, char name[])
+void set_name(int id, char name[])				//to set names to each client 
 {
 	for(int i=0; i<NUM_CLIENTS; i++)
 	{
@@ -153,10 +157,10 @@ void set_name(int id, char name[])
 	}	
 }
 
-string color(int code){
+string color(int code){							//choosing colors to display for each client 
 	return colors[code%NUM_COLORS];
 }
-// Broadcast message to all clients except the sender
+// Broadcast message to all clients except the sender (overloaded function)
 int broadcast_message(string message, int sender_id)
 {
 	char temp[MAX_LEN];
@@ -164,21 +168,17 @@ int broadcast_message(string message, int sender_id)
 	for(int i=0; i<NUM_CLIENTS; i++)
 	{
 		if(clients[i]->get_id()!=sender_id)
-		{
 			send(clients[i]->get_socket(),temp,sizeof(temp),0);
-		}
 	}		
 }
 
-// Broadcast a number to all clients except the sender
+// Broadcast a number to all clients except the sender  (overloaded function)
 int broadcast_message(int num, int sender_id)
 {
 	for(int i=0; i<NUM_CLIENTS; i++)
 	{
 		if(clients[i]->get_id()!=sender_id)
-		{
 			send(clients[i]->get_socket(),&num,sizeof(num),0);
-		}
 	}		
 }
 
@@ -189,8 +189,8 @@ void end_connection(int id)
 		if(clients[i]->get_id()==id)	
 		{
 			lock_guard<mutex> guard(clients_mtx);
-			clients[i]->th.detach();
-			delete clients[i];
+			clients[i]->th.detach();	//detach thread of the particular client
+			delete clients[i];			//delete the element of the object pointer array
 			break;
 		}
 	}				
@@ -199,22 +199,22 @@ void end_connection(int id)
 void handle_client(int client_socket, int id)
 {
 	char name[MAX_LEN],str[MAX_LEN];
-	recv(client_socket,name,sizeof(name),0);
-	set_name(id,name);	
-
+	recv(client_socket,name,sizeof(name),0);		//receive name from the client 
+	set_name(id,name);								//set client name 
 	// Display welcome message
 	string welcome_message=string(name)+string(" has joined");
 	broadcast_message("#NULL",id);	
 	broadcast_message(id,id);								
-	broadcast_message(welcome_message,id);	
+	broadcast_message(welcome_message,id);		//prints welcome message when new client joins
 	server::shared_print(color(id)+welcome_message+def_col);
 	
 	while(1)
 	{
+		//collect received data from the client
 		int bytes_received=recv(client_socket,str,sizeof(str),0);
 		if(bytes_received<=0)
 			return;
-		if(strcmp(str,"#exit")==0)
+		if(strcmp(str,"#exit")==0)		//if a client wants to leave the chatroom and send exit message
 		{
 			// Display leaving message
 			string message=string(name)+string(" has left");		
@@ -222,9 +222,10 @@ void handle_client(int client_socket, int id)
 			broadcast_message(id,id);						
 			broadcast_message(message,id);
 			server::shared_print(color(id)+message+def_col);
-			end_connection(id);							
+			end_connection(id);			//end connection of the particular client				
 			return;
 		}
+		//print the received messages from the various clients connected to the server
 		broadcast_message(string(name),id);					
 		broadcast_message(id,id);		
 		broadcast_message(string(str),id);
